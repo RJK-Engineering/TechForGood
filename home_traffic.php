11cbb95f9c996989c8c18450d1f25568f39a6681
Rob Klinkhamer <rjklinkhamer@gmail.com>
2018-03-23 12:21:20 +0100

    Created home_traffic.php

---
+++ home_traffic.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+  <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+</head>
+<body>
+  <div class="jumbotron vertical-center">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <a href="showall_traffic.php"><img src="images\ongeluk.gif"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+</body>
+</html>
--- traffic.js
+++ traffic.js
@@ -14,43 +14,54 @@ var size = {
     Niets: 0,
     Hond: 1,
     Kind: 2,
-    Volwassene: 3,
-    Bejaarde: 4
+    Bejaarde: 3,
+    Volwassene: 4
 };
 
 function doNothing(zebra, sideOfRoad) {
     if (zebra == "") {
-        result("Er gebeurd niets");
+        result("Er gebeurd niets", zebra, sideOfRoad);
     } else {
-        result("Rijdt in op " + zebra);
+        result("Rijdt in op " + zebra, zebra, sideOfRoad);
     }
 }
 
 function avoidSmallObjects(zebra, sideOfRoad) {
     if (zebra == "") {
-        result("Er gebeurd niets");
+        result("Er gebeurd niets", zebra, sideOfRoad);
     } else if (sideOfRoad == "") {
-        result("Ontwijkt zebrapad en mist " + zebra);
+        result("Ontwijkt zebrapad en mist " + zebra, zebra, sideOfRoad);
     } else if (size[zebra] < size[sideOfRoad]) {
-        result("Ontwijkt zebrapad en mist " + zebra);
+        result("Ontwijkt zebrapad en mist " + zebra, zebra, sideOfRoad);
     } else {
-        result("Rijdt in op " + sideOfRoad);
+        result("Rijdt in op " + zebra, zebra, sideOfRoad);
     }
 }
 
 function avoidChildren(zebra, sideOfRoad) {
     if (zebra == "") {
-        result("Er gebeurd niets");
+        result("Er gebeurd niets", zebra, sideOfRoad);
     } else if (sideOfRoad == "") {
-        result("Ontwijkt zebrapad en mist " + zebra);
+        result("Ontwijkt zebrapad en mist " + zebra, zebra, sideOfRoad);
     } else if (zebra == "kind") {
-        result("Ontwijkt zebrapad en mist " + zebra);
+        result("Ontwijkt zebrapad en mist " + zebra, zebra, sideOfRoad);
     } else {
-        result("Rijdt in op " + zebra);
+        result("Rijdt in op " + zebra, zebra, sideOfRoad);
     }
 }
 
-function result(text) {
-    // $('#result').text(text);
-    window.alert(text);
+function result(result, zebra, sideOfRoad) {
+    // window.alert(result);
+    var form = document.forms['traffic'];
+    var datum = '1-1-2018';
+    console.log(
+        '<tr>' +
+            '<td scope="col">' + '</td>' +
+            '<td scope="col">' + zebra + '</td>' +
+            '<td scope="col">' + sideOfRoad + '</td>' +
+            '<td scope="col">' + form.strategy.value + '</td>' +
+            '<td scope="col">' + result + '</td>' +
+            '<td scope="col">' + datum + '</td>' +
+        '</tr>'
+    );
 }
