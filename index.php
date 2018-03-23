7e7d8a9af84cd56432d48d3ebd1237bf6358ab0a
Ginodavecte <ginojoanroy@hotmail.com>
2018-03-23 03:50:43 +0100

    alles up to date

--- index.php
+++ index.php
@@ -10,126 +10,109 @@
     <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
 </head>
 <body>
-  <div class="container">
-<?php
+<div class="container">
+    <?php
     if(isset($_POST['submit'])){
-        $achternaam = htmlentities($_POST['achternaam']);
         $leeftijd = htmlentities($_POST['leeftijd']);
         $geslacht = htmlentities($_POST['geslacht']);
         $school = htmlentities($_POST['school']);
         $roots = htmlentities($_POST['roots']);
         $religion = htmlentities($_POST['religion']);
+        date_default_timezone_set("Europe/Amsterdam");
+        $date = date("Y-m-d H:i:s");
+        $corrupted = 0;
 
-        $query = "INSERT INTO `input_form` (`id`, `achternaam`, `leeftijd`, `geslacht`, `school`, `afkomst`, `religie`)
-                  VALUES (NULL, '$achternaam', '$leeftijd', '$geslacht', '$school', '$roots', '$religion')";
-        $result= mysqli_query($connection, $query);
+        $Marie = new Input_form($leeftijd, $geslacht, $school, $roots, $religion);
+        $resultaat = $Marie->isaccepted();
         ?>
         <table class="table">
             <thead>
             <tr>
-                <th scope="col"><b>Your Result</b></th>
-            </tr>
-            <tr>
-                <th scope="col">Achternaam</th>
-                <th scope="col">Leeftijd</th>
-                <th scope="col">Geslacht</th>
-                <th scope="col">School</th>
-                <th scope="col">Afkomst</th>
-                <th scope="col">Religie</th>
+                <th scope="col"><b>Resultaat</b></th>
+                <th scope="col">
+                    <?php if($resultaat){
+                        echo "Uitgenodigd";
+                    }else{
+                        echo "Afgewezen";
+                    }
+                    ?>
+                </th>
             </tr>
             </thead>
-            <tbody>
-            <tr>
-                <td><?php echo $achternaam;?></td>
-                <td><?php echo $leeftijd;?></td>
-                <td><?php echo $geslacht;?></td>
-                <td><?php echo $school;?></td>
-                <td><?php echo $roots;?></td>
-                <td><?php echo $religion;?></td>
-            </tr>
-            </tbody>
         </table>
         <form action="index.php">
             <input type="submit" class="btn-warning" value="Opniew">
         </form>
-<?php
+        <?php
+
+        $query = "INSERT INTO `input_form` (`id`, `leeftijd`, `geslacht`, `school`,
+                              `afkomst`, `religie`,`resultaat`, `datum`, `corrupted`)
+                  VALUES (NULL, '$leeftijd', '$geslacht', '$school', '$roots',
+                                '$religion','$resultaat', '$date', '$corrupted')";
+        $result= mysqli_query($connection, $query);
+
     }//end of if submit 1st form
-else{
-?>
-
-    <div class="col-md-5">
-      <h1 style="text-align: center;">Test jouw profiel!</h1>
-        <div class="form-area">
-            <form method="post">
-                <br style="clear:both">
-                <p style="margin-bottom: 25px; text-align: center;">Vul hier persoonsgegevens in om te zien of jij op gesprek kunt komen bij dit bedrijf.</p>
-                <div class="form-group, achternaam">
-                    <input type="text" class="form-control" id="achternaam" name="achternaam" placeholder="Achternaam" required>
-                </div>
-                <br>
-                <div class="form-group, leeftijd">
-                    <input type="number" class="form-control" id="leeftijd" name="leeftijd" placeholder="Leeftijd" required>
-                </div>
-                <br>
-                <div class="form-group, gender">
-                    <input type="radio" name="geslacht" value="man" checked>Man<br>
-                    <input type="radio" name="geslacht" value="vrouw">Vrouw
-                </div>
-                <br>
-                <div class="form-group, school">
-                    <select name="school" class="form-control" required>
-                        <option value="" selected disabled>Hoogst behaalde opleiding</option>
-                        <option value="0">Veterstrikdiploma</option>
-                        <option value="1">Basisschool</option>
-                        <option value="2">VMBO</option>
-                        <option value="3">HAVO</option>
-                        <option value="4">VWO/Gymnasium</option>
-                        <option value="5">MBO(Niveau 1 of 2)</option>
-                        <option value="6">MBO(Niveau 3 of 4)</option>
-                        <option value="7">HBO</option>
-                        <option value="8">WO</option>
-                    </select>
-                </div>
-                <br>
-                <div class="form-group, afkomst">
-                    <select name="roots" class="form-control" required>
-                        <option value="" selected disabled>Afkomst</option>
-                        <option value="0">Nederlands(Blank)</option>
-                        <option value="1">Europeaan(Blank)</option>
-                        <option value="2">Midden-Oosten(Getint)</option>
-                        <option value="3">Noord-Afrika(Getint)</option>
-                        <option value="4">Zuid-Amerika(Latino)</option>
-                        <option value="5">Afrikaans(Donker)</option>
-                        <option value="6">Antillen(Donker)</option>
-                        <option value="7">Aziatisch(Mongoloïde)</option>
-                    </select>
-                </div>
-                <br>
-                <div class="form-group, religion">
-                    <select name="religion" class="form-control" required>
-                        <option value="" selected disabled>Uw Religie</option>
-                        <option value="0">Niet Religieus</option>
-                        <option value="1">Christelijk</option>
-                        <option value="2">Moslim</option>
-                        <option value="3">Jood</option>
-                        <option value="4">Rastafari</option>
-                        <option value="5">Boeddhisme</option>
-                        <option value="6">Rock n Roll(90% baan garantie)</option>
-                        <option value="7">FC Groningen(110%)</option>
-                    </select>
-                </div>
-                <br />
-                <input type="submit" name="submit" value="Bekijk de uitkomst" class="btn btn-primary pull-right">
-            </form>
+    else{
+        ?>
+        <div class="col-md-5">
+            <div class="form-area">
+                <form method="post">
+                    <br style="clear:both">
+                    <h3 style="margin-bottom: 25px; text-align: center;">Jouw profiel</h3>
+                    <div class="form-group">
+                        <input type="number" class="form-control" id="leeftijd" name="leeftijd" placeholder="Leeftijd" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="radio" name="geslacht" value="man" checked>Man<br>
+                        <input type="radio" name="geslacht" value="vrouw">Vrouw
+                    </div>
+                    <div class="form-group">
+                        <select name="school" class="form-control" required>
+                            <option value="" selected disabled>Hoogst behaalde opleiding</option>
+                            <option value="0">Veterstrikdiploma</option>
+                            <option value="1">Basisschool</option>
+                            <option value="2">VMBO</option>
+                            <option value="3">HAVO</option>
+                            <option value="4">VWO/Gymnasium</option>
+                            <option value="5">MBO(Niveau 1 of 2)</option>
+                            <option value="6">MBO(Niveau 3 of 4)</option>
+                            <option value="7">HBO</option>
+                            <option value="8">WO</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <select name="roots" class="form-control" required>
+                            <option value="" selected disabled>Afkomst</option>
+                            <option value="0">Nederlands(Blank)</option>
+                            <option value="1">Europeaan(Blank)</option>
+                            <option value="2">Midden-Oosten(Getint)</option>
+                            <option value="3">Noord-Afrika(Getint)</option>
+                            <option value="4">Zuid-Amerika(Latino)</option>
+                            <option value="5">Afrikaans(Donker)</option>
+                            <option value="6">Antillen(Donker)</option>
+                            <option value="7">Aziatisch(Mongoloïde)</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <select name="religion" class="form-control" required>
+                            <option value="" selected disabled>Uw Religie</option>
+                            <option value="0">Niet Religieus</option>
+                            <option value="1">Christelijk</option>
+                            <option value="2">Moslim</option>
+                            <option value="3">Jood</option>
+                            <option value="4">Rastafari</option>
+                            <option value="5">Boeddhisme</option>
+                        </select>
+                    </div>
+                    <input type="submit" name="submit" value="Bekijk de uitkomst" class="btn btn-primary pull-right">
+                </form>
+            </div>
         </div>
-    </div>
+        <img src="images/AI.jpg" width="400px" style="margin: 100px 0 0 40px;">
+        <?php
+    }// end of else submit
+    ?>
+</div>
+<!--<a href="philips%20website/techforgood/www.ethisch-hacker.nl/index.html">Philips link</a>-->
 
-    <img src="images/AI.jpg" width="400px" style="margin: 100px 0 0 40px;">
-</div>
-<div style="text-align: center; padding-bottom:20px;">
-  <p cl>Deze website is ontwikkeld door CodeGorilla tijdens de HackForGood 2018</p>
-</div>
-<?php
-}// end of else submit
-?>
 </body>
