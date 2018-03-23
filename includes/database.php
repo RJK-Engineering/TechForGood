303e891018958bf7061ebb1872294cc161a9672d
Ginodavecte <ginojoanroy@hotmail.com>
2018-03-23 11:26:54 +0100

    Philips en Philips2

--- includes/database.php
+++ includes/database.php
@@ -1,9 +1,9 @@
 <?php
 //database opvragen
-$dbhost = "db.ginojoanroy.nl";
-$dbuser = "md427962db407505";
-$dbpass = "95dGJPCl";
-$dbname = "md427962db407505";
+$dbhost = "127.0.0.1:3307";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "techforgood";
 $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if(mysqli_connect_errno()) {
     die("database query failed: " .
